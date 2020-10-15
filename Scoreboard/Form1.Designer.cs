1d0bc29434d13353fa977e03525894a0b1a2c09f
Alex Gonzales <agonzalesband99@gmail.com>
2020-10-14 19:28:31 -0500

    1.0

---
+++ Scoreboard/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Scoreboard
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.image1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.plusOneAway = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.awayTeam = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.awayTeamResult = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.minusOne = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.homeTeamResult = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.awayTeam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image1
+            // 
+            this.image1.Location = new System.Drawing.Point(3, 370);
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(319, 68);
+            this.image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image1.TabIndex = 0;
+            this.image1.TabStop = false;
+            this.image1.Click += new System.EventHandler(this.image1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Home Team Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // plusOneAway
+            // 
+            this.plusOneAway.Location = new System.Drawing.Point(21, 99);
+            this.plusOneAway.Name = "plusOneAway";
+            this.plusOneAway.Size = new System.Drawing.Size(112, 40);
+            this.plusOneAway.TabIndex = 2;
+            this.plusOneAway.Text = "Away Team Upload";
+            this.plusOneAway.UseVisualStyleBackColor = true;
+            this.plusOneAway.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(267, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "+2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(267, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "+3";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Away Team (Add)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // awayTeam
+            // 
+            this.awayTeam.Location = new System.Drawing.Point(319, 370);
+            this.awayTeam.Name = "awayTeam";
+            this.awayTeam.Size = new System.Drawing.Size(319, 68);
+            this.awayTeam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.awayTeam.TabIndex = 6;
+            this.awayTeam.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(267, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "+1";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Scoreboard.Properties.Resources.CBN_Sports_logo;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-44, -11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(267, 255);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(401, 255);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 11;
+            this.btnEnd.Text = "Stop";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // awayTeamResult
+            // 
+            this.awayTeamResult.AutoSize = true;
+            this.awayTeamResult.BackColor = System.Drawing.Color.Transparent;
+            this.awayTeamResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.awayTeamResult.ForeColor = System.Drawing.Color.Black;
+            this.awayTeamResult.Location = new System.Drawing.Point(282, 389);
+            this.awayTeamResult.Name = "awayTeamResult";
+            this.awayTeamResult.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.awayTeamResult.Size = new System.Drawing.Size(30, 31);
+            this.awayTeamResult.TabIndex = 12;
+            this.awayTeamResult.Text = "0";
+            this.awayTeamResult.Click += new System.EventHandler(this.awayTeamResult_Click_1);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Red;
+            this.resetButton.Location = new System.Drawing.Point(21, 255);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 13;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // minusOne
+            // 
+            this.minusOne.Location = new System.Drawing.Point(374, 42);
+            this.minusOne.Name = "minusOne";
+            this.minusOne.Size = new System.Drawing.Size(75, 23);
+            this.minusOne.TabIndex = 14;
+            this.minusOne.Text = "-1";
+            this.minusOne.UseVisualStyleBackColor = true;
+            this.minusOne.Click += new System.EventHandler(this.minusOne_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(374, 90);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "-2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(374, 136);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "-3";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(662, 42);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "-1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // homeTeamResult
+            // 
+            this.homeTeamResult.AutoSize = true;
+            this.homeTeamResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.homeTeamResult.Location = new System.Drawing.Point(599, 389);
+            this.homeTeamResult.Name = "homeTeamResult";
+            this.homeTeamResult.Size = new System.Drawing.Size(30, 31);
+            this.homeTeamResult.TabIndex = 19;
+            this.homeTeamResult.Text = "0";
+            this.homeTeamResult.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(554, 42);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "+1";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(551, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Home Teams (Add)";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.homeTeamResult);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.minusOne);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.awayTeamResult);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.awayTeam);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.plusOneAway);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.image1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "form1";
+            this.Text = "CBN Scorebar Version 1.0 BETA";
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.awayTeam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button plusOneAway;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox awayTeam;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label awayTeamResult;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button minusOne;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label homeTeamResult;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Scoreboard/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scoreboard
+{
+    public partial class form1 : Form
+    {
+       
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String imageLocation = "";
+            try
+            {
+               
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "PNG files(*.png)|*.png| jpg files(*.jpg)|*.jpg|  All Files('.')|'.'";
+
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+
+                    awayTeam.ImageLocation = imageLocation;
+                }
+            
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String imageLocation2 = "";
+            try
+            {
+
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "PNG files(*.png)|*.png| jpg files(*.jpg)|*.jpg|  All Files('.')|'.'";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation2 = dialog.FileName;
+
+                    image1.ImageLocation = imageLocation2;
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void image1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1; // 1 second interval
+
+        }
+
+        private void awayTeamResult_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)// + 1 Button
+        {
+
+            Button button = (Button)sender;
+
+            int plusOne = Convert.ToInt32(button.Text);
+            int textboxScore = Convert.ToInt32(awayTeamResult.Text);
+            int totalScore = plusOne + textboxScore;
+
+
+            awayTeamResult.Text = totalScore.ToString();
+           
+        }
+
+        private void button5_Click(object sender, EventArgs e) 
+        {
+            Button button = (Button)sender;
+
+            awayTeamResult.Text = "0";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int plusOne = Convert.ToInt32(button.Text);
+            int textboxScore = Convert.ToInt32(awayTeamResult.Text);
+            int totalScore = plusOne + textboxScore;
+
+
+            awayTeamResult.Text = totalScore.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int plusOne = Convert.ToInt32(button.Text);
+            int textboxScore = Convert.ToInt32(awayTeamResult.Text);
+            int totalScore = plusOne + textboxScore;
+
+
+            awayTeamResult.Text = totalScore.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int textboxScore = Convert.ToInt32(awayTeamResult.Text);
+            int totalScore = textboxScore - 2;
+
+            if (awayTeamResult.Text == "0")
+                awayTeamResult.Text = "0";
+            else
+                awayTeamResult.Text = totalScore.ToString();
+
+        }
+
+        private void minusOne_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int textboxScore = Convert.ToInt32(awayTeamResult.Text);
+            int totalScore =  textboxScore - 1 ; // fix this line make sure variable
+
+            if (awayTeamResult.Text == "0")
+            {
+                awayTeamResult.Text = "0";
+            }
+            else
+                awayTeamResult.Text = totalScore.ToString();
+
+        }
+
+        private void awayTeamResult_Click_1(object sender, EventArgs e)
+        {
+           // awayTeamResult.BackColor = Color.Transparent;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int textboxScore = Convert.ToInt32(awayTeamResult.Text);
+            int totalScore = textboxScore - 3;
+
+            if (awayTeamResult.Text == "0")
+                awayTeamResult.Text = "0";
+            else
+                awayTeamResult.Text = totalScore.ToString();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int textboxScore = Convert.ToInt32(homeTeamResult.Text);
+            int totalScore = textboxScore - 1; // fix this line make sure variable
+
+            if (homeTeamResult.Text == "0")
+            {
+                homeTeamResult.Text = "0";
+            }
+            else
+                homeTeamResult.Text = totalScore.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            int plusOne = Convert.ToInt32(button.Text);
+            int textboxScore = Convert.ToInt32(homeTeamResult.Text);
+            int totalScore = plusOne + textboxScore;
+
+
+            homeTeamResult.Text = totalScore.ToString();
+        }
+    }
+}
